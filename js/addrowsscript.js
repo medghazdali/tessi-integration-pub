da679ab9472ce63edd63c862db9e425c4547eb29
med <med.ghazdali@gmail.com>
2021-04-01 10:43:27 +0100

    add pages

--- js/addrowsscript.js
+++ js/addrowsscript.js
@@ -1,4 +1,26 @@
 
+$(document).on("click", ".btn-parames-definition-add", function (event) {
+
+    $(".row-of-fileds-trakins").append('<div class="row mb-2 item-trakin align-items-center">' +
+        ' <div class="col-auto  col-10"><input type="text" class="form-control"></div>' +
+        ' <div class="col-2  justify-content-end flex-xxl-row-reverse"> ' +
+        ' <button class="btn-lien btn-lien-remove btn btn-sm btn-danger justify-content-center" type="button"><i class="fa fa-times" aria-hidden="true"></i></button>' +
+        ' <button class="btn-lien btn-lien-add btn btn-sm btn-primary" type="button"> <i class="fa fa-plus" aria-hidden="true"></i></button>' +
+        ' </div></div>'
+    )
+})
+
+$(document).on("click", ".btn-parames-definition-remove", function (event) {
+
+    var answer = confirm('Are you sure that you want to do this?');
+    if (!answer) {
+        event.preventDefault();
+    } else {
+        $(this).parent().parent().remove();
+
+    }
+})
+
 $(document).on("click", ".btn-variable-add", function (event) {
 
     $(".row-of-fileds-variables").append('<div class="row mb-2 item-trakin align-items-center">' +
